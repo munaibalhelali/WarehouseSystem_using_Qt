d6b7b2c806eb5e261101ad840a27d29304486bcf
munaibalhelali <moneebalhelaly@yahoo.com>
2023-01-05 22:54:46 +0100

    implemented addNewProductDialog

--- addnewproductdialog.cpp
+++ addnewproductdialog.cpp
@@ -1,5 +1,6 @@
 #include "addnewproductdialog.h"
 #include "ui_addnewproductdialog.h"
+#include <QMessageBox>
 
 AddNewProductDialog::AddNewProductDialog(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +12,92 @@ AddNewProductDialog::AddNewProductDialog(QWidget *parent) :
 AddNewProductDialog::~AddNewProductDialog()
 {
     delete ui;
+}
+
+void AddNewProductDialog::on_saveButton_clicked()
+{
+
+
+    QString inName = ui->productNameLineEdit->text();
+    QString inManufacturer = ui->ManufacturerLineEdit->text();
+    int inSize = ui->sizeComboBox->currentIndex();
+    int inCategory = ui->CategoryComboBox->currentIndex();
+    QString inExpiryDate = ui->expiryDateEdit->text();
+
+    if(!inName.isEmpty() && !inManufacturer.isEmpty()
+            && inSize != 0 && inCategory != 0 && !inExpiryDate.isEmpty()){
+        name = inName;
+        manufacturer = inManufacturer;
+        expiryDate = inExpiryDate;
+        size = ui->sizeComboBox->currentText();
+        category = ui->CategoryComboBox->currentText();
+        accept();
+
+    }else{
+        if(inName.isEmpty()){
+            ui->productNameLineEdit->setStyleSheet(warningStyle);
+        }else{
+            ui->productNameLineEdit->setStyleSheet(acceptStyle);
+        }
+        if(inManufacturer.isEmpty()){
+            ui->ManufacturerLineEdit->setStyleSheet(warningStyle);
+        }else{
+            ui->ManufacturerLineEdit->setStyleSheet(acceptStyle);
+        }
+        if(inSize == 0){
+            ui->sizeComboBox->setStyleSheet(warningStyle);
+        }else{
+            ui->sizeComboBox->setStyleSheet(acceptStyle);
+        }
+        if(inCategory == 0){
+            ui->CategoryComboBox->setStyleSheet(warningStyle);
+        }else{
+            ui->CategoryComboBox->setStyleSheet(acceptStyle);
+        }
+        if(inExpiryDate.isEmpty()){
+            ui->expiryDateEdit->setStyleSheet(warningStyle);
+        }else{
+            ui->expiryDateEdit->setStyleSheet(acceptStyle);
+        }
+
+        auto ret = QMessageBox::warning(this, "Incorrect data", "Please make sure you provided all needed data.", QMessageBox::Discard, QMessageBox::Retry);
+        if(ret == QMessageBox::Discard){
+            reject();
+        }
+    }
+
+}
+
+void AddNewProductDialog::on_cancelButton_clicked()
+{
+    reject();
+
+}
+
+void AddNewProductDialog::on_sizeComboBox_currentIndexChanged(int index)
+{
+    if(index != 0)
+        ui->sizeComboBox->setStyleSheet(acceptStyle);
+}
+
+void AddNewProductDialog::on_CategoryComboBox_currentIndexChanged(int index)
+{
+    if(index != 0)
+        ui->CategoryComboBox->setStyleSheet(acceptStyle);
+}
+
+void AddNewProductDialog::on_productNameLineEdit_editingFinished()
+{
+    if(!ui->productNameLineEdit->text().isEmpty())
+        ui->productNameLineEdit->setStyleSheet(acceptStyle);
+    else
+        ui->productNameLineEdit->setStyleSheet(warningStyle);
+}
+
+void AddNewProductDialog::on_ManufacturerLineEdit_editingFinished()
+{
+    if(!ui->ManufacturerLineEdit->text().isEmpty())
+        ui->ManufacturerLineEdit->setStyleSheet(acceptStyle);
+    else
+        ui->ManufacturerLineEdit->setStyleSheet(warningStyle);
 }
--- addnewproductdialog.h
+++ addnewproductdialog.h
@@ -15,8 +15,29 @@ public:
     explicit AddNewProductDialog(QWidget *parent = nullptr);
     ~AddNewProductDialog();
 
+private slots:
+    void on_saveButton_clicked();
+
+    void on_cancelButton_clicked();
+
+    void on_sizeComboBox_currentIndexChanged(int index);
+
+    void on_CategoryComboBox_currentIndexChanged(int index);
+
+    void on_productNameLineEdit_editingFinished();
+
+    void on_ManufacturerLineEdit_editingFinished();
+
 private:
     Ui::AddNewProductDialog *ui;
+    QString name;
+    QString manufacturer;
+    QString size;
+    QString category;
+    QString expiryDate;
+
+    QString warningStyle = "border-color: rgb(255,106,77); border-style: solid; border-width: 2px;";
+    QString acceptStyle = "border-color: rgb(166,255,77); border-style: solid; border-width: 2px;";
 };
 
 #endif // ADDNEWPRODUCTDIALOG_H

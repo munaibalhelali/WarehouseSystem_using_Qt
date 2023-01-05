2336e0de9efa61d43ea31053d2fb0808d9ae65b2
munaibalhelali <moneebalhelaly@yahoo.com>
2023-01-05 22:55:07 +0100

    implemented addNewUserDialog

--- addnewuserdialog.cpp
+++ addnewuserdialog.cpp
@@ -1,14 +1,49 @@
 #include "addnewuserdialog.h"
 #include "ui_addnewuserdialog.h"
+#include <QMessageBox>
 
 AddNewUserDialog::AddNewUserDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddNewUserDialog)
 {
     ui->setupUi(this);
+    ui->adminRadioButton->setChecked(true);
 }
 
 AddNewUserDialog::~AddNewUserDialog()
 {
     delete ui;
+}
+
+void AddNewUserDialog::on_saveButton_clicked()
+{
+    QString inName =  ui->nameLineEdit->text();
+    QString inPassword = ui->passwordLineEdit->text();
+    QString inConfPassword = ui->confirmPasswordLineEdit->text();
+    bool adminRadio = ui->adminRadioButton->isChecked();
+    bool staffRadio = ui->staffRadioButton->isChecked();
+    if (!inName.isEmpty() && !inPassword.isEmpty() && inPassword == inConfPassword){
+        name = inName;
+        Password = inPassword;
+        if(adminRadio){
+            role = ui->adminRadioButton->text();
+        }else if(staffRadio){
+            role = ui->staffRadioButton->text();
+        }
+        accept();
+    }else if(!inPassword.isEmpty() && inPassword != inConfPassword){
+        ui->confirmPasswordLineEdit->setStyleSheet("background:rgb(255,106,77)");
+    }else{
+        auto ret = QMessageBox::warning(this, "Incorrect data", "Please make sure you provided all needed data.", QMessageBox::Discard, QMessageBox::Retry);
+        if(ret == QMessageBox::Discard){
+            reject();
+        }
+    }
+
+}
+
+void AddNewUserDialog::on_cancelButton_clicked()
+{
+    reject();
+
 }
--- addnewuserdialog.h
+++ addnewuserdialog.h
@@ -15,8 +15,18 @@ public:
     explicit AddNewUserDialog(QWidget *parent = nullptr);
     ~AddNewUserDialog();
 
+private slots:
+    void on_saveButton_clicked();
+
+    void on_cancelButton_clicked();
+
 private:
     Ui::AddNewUserDialog *ui;
+    QString name;
+    QString Password;
+    QString role;
+
+    QList<QString> availableRoles{"Admin", "Staff"};
 };
 
 #endif // ADDNEWUSERDIALOG_H

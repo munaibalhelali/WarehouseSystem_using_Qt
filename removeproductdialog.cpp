fc300a1d185e9adb501f1ba0b9009c145ee9620f
munaibalhelali <moneebalhelaly@yahoo.com>
2023-01-07 20:22:40 +0100

    implemented removeProductDialog

--- removeproductdialog.cpp
+++ removeproductdialog.cpp
@@ -1,6 +1,6 @@
 #include "removeproductdialog.h"
 #include "ui_removeproductdialog.h"
-
+#include <QMessageBox>
 RemoveProductDialog::RemoveProductDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::RemoveProductDialog)
@@ -11,4 +11,28 @@ RemoveProductDialog::RemoveProductDialog(QWidget *parent) :
 RemoveProductDialog::~RemoveProductDialog()
 {
     delete ui;
+}
+
+void RemoveProductDialog::on_removePushButton_clicked()
+{
+    bool ErrFlag = false;
+
+    if(ErrFlag){
+        QMessageBox::warning(this, "Missing information!", "Please make sure you provided all the required information.", QMessageBox::Ok);
+    }else{
+        accept();
+    }
+}
+
+void RemoveProductDialog::on_cancelPushButton_clicked()
+{
+    auto ret = QMessageBox::warning(this, "Cancel remove product?", "Do you want to discard this task?", QMessageBox::Discard, QMessageBox::Cancel|QMessageBox::Default|QMessageBox::Escape);
+    if(ret == QMessageBox::Discard){
+        reject();
+    }
+}
+
+void RemoveProductDialog::on_searchPushButton_clicked()
+{
+
 }
--- removeproductdialog.h
+++ removeproductdialog.h
@@ -15,6 +15,13 @@ public:
     explicit RemoveProductDialog(QWidget *parent = nullptr);
     ~RemoveProductDialog();
 
+private slots:
+    void on_removePushButton_clicked();
+
+    void on_cancelPushButton_clicked();
+
+    void on_searchPushButton_clicked();
+
 private:
     Ui::RemoveProductDialog *ui;
 };

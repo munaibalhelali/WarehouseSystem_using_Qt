e204f402e5557312b55ad418ea66d182bad602d4
munaibalhelali <moneebalhelaly@yahoo.com>
2023-01-18 09:18:39 +0100

    added comments

--- include/warehouseSystem/driver.hpp
+++ include/warehouseSystem/driver.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file driver.hpp
+ * @author Munaib Al-Helali (moneebalhelaly@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #ifndef DRIVER_
 #define DRIVER_
 #include <iostream>
@@ -15,10 +25,37 @@ class Driver
     Driver(){}
     
 public:
+    /**
+     * @brief Construct a new Driver object
+     * 
+     * @param user 
+     */
     Driver(Person user);
+
+    /**
+     * @brief Destroy the Driver object
+     * 
+     */
     ~Driver();
+
+    /**
+     * @brief Set the Current User object
+     * 
+     * @param user 
+     */
     void setCurrentUser(Person user);
+
+    /**
+     * @brief Get the Current User object
+     * 
+     * @return Person 
+     */
     Person getCurrentUser();
+
+    /**
+     * @brief 
+     * 
+     */
     void run();
 
 };
